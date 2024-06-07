9900b4dfdf9f9f21cbdc343a5d37f24031c4dda0
kcherenkovv <kcherenkov9888@mail.ru>
2024-06-07 16:06:08 +0300

    Incorporating Dataset Changes & Creating Core Project Files and Configurations

---
+++ scripts/image_processing.py
@@ -0,0 +1,35 @@
+import io
+import torch
+import streamlit as st
+from PIL import Image
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+
+def generate_image_description(image_content):
+    model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    max_length = 16
+    num_beams = 4
+    gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+    if isinstance(image_content, str):  # Assuming image_content is a file path
+        img = Image.open(image_content)
+    elif isinstance(image_content, bytes):  # Assuming image_content is already bytes
+        img = Image.open(io.BytesIO(image_content))
+    elif isinstance(image_content, Image.Image):  # If it's already a PIL.Image.Image instance
+        img = image_content
+    else:
+        raise ValueError("Unsupported image content type")
+
+    if img.mode != "RGB":
+        img = img.convert('RGB')
+
+    pixel_values = feature_extractor(images=[img], return_tensors="pt").pixel_values.to(device)
+    output_ids = model.generate(pixel_values, **gen_kwargs)
+    description = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+
+    return description
---
+++ scripts/main.py
@@ -0,0 +1,25 @@
+import io
+import streamlit as st
+from PIL import Image
+from image_processing import generate_image_description
+
+def load_image():
+    uploaded_file = st.file_uploader("Загрузите изображение для описания")
+    if uploaded_file is not None:
+        img = Image.open(io.BytesIO(uploaded_file.read()))
+        if img.mode != "RGB":
+            img = img.convert('RGB')
+        st.image(img, caption='Uploaded Image.', use_column_width=True)
+        return img
+    return None
+
+def main():
+    st.title('Описание изображения с помощью Hugging Face модели')
+    uploaded_image = load_image()
+    
+    if uploaded_image:
+        description = generate_image_description(uploaded_image)
+        st.subheader(f'Описание изображения: {description}')
+
+if __name__ == '__main__':
+    main()
